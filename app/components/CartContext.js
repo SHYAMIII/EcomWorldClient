29c189e96c618f6b0583359881a3c166ebbe9f08
shyam <shyamghosh2004@gmail.com>
2025-02-20 11:41:12 +0530

    kani

--- app/components/CartContext.js
+++ app/components/CartContext.js
@@ -4,23 +4,23 @@ import { createContext, useEffect, useState } from "react";
 export const CartContext = createContext({});
 
 export function CartProvider({ children }) {
-    const [cartProducts, setCartProducts] = useState(JSON.parse(localStorage.getItem('cartProducts')) || []);
-    useEffect(() => {
-        // Check if localStorage is available
-        if (typeof window !== 'undefined' && window.localStorage) {
-          const storedCart = localStorage.getItem('cartProducts');
-          if (storedCart) {
-            setCartProducts(JSON.parse(storedCart));
-          }
-        }
-      }, []);
+    const [cartProducts, setCartProducts] = useState([]);
+    // useEffect(() => {
+    //     // Check if localStorage is available
+    //     if (typeof window !== 'undefined' && window.localStorage) {
+    //       const storedCart = localStorage.getItem('cartProducts');
+    //       if (storedCart) {
+    //         setCartProducts(JSON.parse(storedCart));
+    //       }
+    //     }
+    //   }, []);
     
-      useEffect(() => {
-        // Update localStorage whenever cartProducts changes
-        if (typeof window !== 'undefined' && window.localStorage) {
-          localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
-        }
-      }, [cartProducts]);
+      // useEffect(() => {
+      //   // Update localStorage whenever cartProducts changes
+      //   if (typeof window !== 'undefined' && window.localStorage) {
+      //     localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+      //   }
+      // }, [cartProducts]);
     
     
     function AddCart(id) {
